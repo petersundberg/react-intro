510f58b92fc90e84c4cf4e0f513a9857d46e0b2c
Peter Sundberg <peter.sundberg67@gmail.com>
2020-09-24 13:44:46 +0200

    code add data to list

--- src/index.js
+++ src/index.js
@@ -115,5 +115,56 @@
 
 //-------------------------------------------------------------------
 
+
 import React from "react";
 import ReactDOM from "react-dom";
+
+class Citylister extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            sweCities: ["Stockholm", "Göteborg", "Lund", "Växjö", "Härnösand"],
+        };
+
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        /* Att direkt modifiera en array går inte i React, 
+        därför kan man lösa det så här */
+
+        // Create a new array based on current state:
+        let sweCities = [...this.state.sweCities];
+
+        // Add item to it
+        sweCities.push(event.target.newcity.value);
+
+        // Set state
+        this.setState({ sweCities });
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.sweCities.map((city) => (
+                        <li>{city}</li>
+                    ))}
+                </ul>
+
+                <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <input name="newcity" type="text" />
+                    <input type="submit" value="Lägg till stad" />
+                </form>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Citylister />, document.getElementById("root"));
+
+
+//------------------------------------------------------
+
+
+
