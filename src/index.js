1bfe6d2b9e622a32d8afc8b0d7486cfdcca9b037
Peter Sundberg <peter.sundberg67@gmail.com>
2020-09-24 14:48:23 +0200

    product list

--- src/index.js
+++ src/index.js
@@ -116,52 +116,52 @@
 //-------------------------------------------------------------------
 
 
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-class Citylister extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            sweCities: ["Stockholm", "Göteborg", "Lund", "Växjö", "Härnösand"],
-        };
+// class Citylister extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             sweCities: ["Stockholm", "Göteborg", "Lund", "Växjö", "Härnösand"],
+//         };
 
-    }
+//     }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        /* Att direkt modifiera en array går inte i React, 
-        därför kan man lösa det så här */
+//     handleSubmit(event) {
+//         event.preventDefault();
+//         /* Att direkt modifiera en array går inte i React, 
+//         därför kan man lösa det så här */
 
-        // Create a new array based on current state:
-        let sweCities = [...this.state.sweCities];
+//         // Create a new array based on current state:
+//         let sweCities = [...this.state.sweCities];
 
-        // Add item to it
-        sweCities.push(event.target.newcity.value);
+//         // Add item to it
+//         sweCities.push(event.target.newcity.value);
 
-        // Set state
-        this.setState({ sweCities });
-    }
+//         // Set state
+//         this.setState({ sweCities });
+//     }
 
-    render() {
-        return (
-            <div>
-                <ul>
-                    {this.state.sweCities.map((city) => (
-                        <li>{city}</li>
-                    ))}
-                </ul>
+//     render() {
+//         return (
+//             <div>
+//                 <ul>
+//                     {this.state.sweCities.map((city) => (
+//                         <li>{city}</li>
+//                     ))}
+//                 </ul>
 
-                <form onSubmit={(e) => this.handleSubmit(e)}>
-                    <input name="newcity" type="text" />
-                    <input type="submit" value="Lägg till stad" />
-                </form>
-            </div>
-        );
-    }
-}
+//                 <form onSubmit={(e) => this.handleSubmit(e)}>
+//                     <input name="newcity" type="text" />
+//                     <input type="submit" value="Lägg till stad" />
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
 
-ReactDOM.render(<Citylister />, document.getElementById("root"));
+// ReactDOM.render(<Citylister />, document.getElementById("root"));
 
 
 //------------------------------------------------------
